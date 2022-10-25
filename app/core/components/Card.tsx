b7f8dfca7659489ce274701851452517bf3a1f1f
beerose <alexsandra.sikora@gmail.com>
2022-10-25 12:55:16 +0800

    improve layout

--- app/core/components/Card.tsx
+++ app/core/components/Card.tsx
@@ -1,5 +1,7 @@
 export const Card: React.FC<any> = ({ children }) => {
   return (
-    <div className="bg-white border-2 border-black rounded-xl p-10 w-3/4 sm:w-2/3">{children}</div>
+    <div className="bg-white border-2 border-black rounded-xl p-10 w-5/6 md:w-2/3 lg:w-1/2">
+      {children}
+    </div>
   )
 }
