240014e39837b9eedd7ed2723315d65813a269ea
beerose <alexsandra.sikora@gmail.com>
2023-02-02 11:28:00 -0300

    Fix db import

--- app/blitz-server.ts
+++ app/blitz-server.ts
@@ -1,7 +1,7 @@
 import { setupBlitzServer } from "@blitzjs/next"
 import { AuthServerPlugin, PrismaStorage } from "@blitzjs/auth"
 import { simpleRolesIsAuthorized } from "@blitzjs/auth"
-import db from "db"
+import db from "../db"
 import { authConfig } from "./blitz-client"
 
 export const { gSSP, gSP, api } = setupBlitzServer({
@@ -13,4 +13,3 @@ export const { gSSP, gSP, api } = setupBlitzServer({
     }),
   ],
 })
-
